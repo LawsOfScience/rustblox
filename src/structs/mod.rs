b93acf8ab240fd0c0fad6586b324b5d0870546ff
LawsOfScience <thesandwichsupreme@gmail.com>
2025-06-30 16:56:18 -0400

    docs: Add descriptions for generic structs

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,12 +1,15 @@
 pub mod user;
 pub mod group;
 
+/// Represents a sorting order for API endpoints
+/// that can use it.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum SortOrder {
     Ascending,
     Descending,
 }
 
+/// Represents a page of data for paginated endpoints.
 #[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct Page<T> {
@@ -15,6 +18,13 @@ pub struct Page<T> {
     pub data: Vec<T>,
 }
 
+/// Convenience struct used for endpoints that return
+/// a response of the form
+/// ```json
+/// {
+///   "data": [ ... ]
+/// }
+/// ```
 #[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct DataWrapper<T> {
     pub data: Vec<T>,
